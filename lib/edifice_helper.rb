c45507729076fc794d78491960f986069fa42d59
Tom Coleman <tom@thesnail.org>
2011-02-14 14:15:03 +1100

    Fixed up invalid name attribute on metas

--- lib/edifice_helper.rb
+++ lib/edifice_helper.rb
@@ -1,9 +1,9 @@
 module EdificeHelper
   # put this in your layout somewhere
   def edifice_meta_tags
-    %(<meta name='edifice.view_path' content='#{view_path_normalized}'/>
-      <meta name='edifice.view_name' content='#{view_name_normalized}'/>
-      <meta name='edifice.layout' content='#{layout_name}'/>).html_safe
+    %(<meta name='edifice-view_path' content='#{view_path_normalized}'/>
+      <meta name='edifice-view_name' content='#{view_name_normalized}'/>
+      <meta name='edifice-layout' content='#{layout_name}'/>).html_safe
   end
   
   # the default classes that get added to the body element when a view renders
