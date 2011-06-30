177b6aad18c866819beea0cbe4229cf6b02395fc
Tom Coleman <tom@thesnail.org>
2011-06-30 16:45:00 +1000

    Changed the responder to return data on update for JSON etc

--- lib/edifice/responder.rb
+++ lib/edifice/responder.rb
@@ -2,9 +2,19 @@
 module Edifice
   class Responder < ActionController::Responder
   protected 
+    # add the :u_e header to xhr error requests
     def to_html
       if controller.request.xhr? && !get? and has_errors? && default_action
         render :action => default_action, :status => :unprocessable_entity, :layout => nil
+      else
+        super
+      end
+    end
+    
+    # actually render something on successful updates
+    def to_format
+      unless get? or has_errors? or post?
+        display resource, :status => :ok
       else
         super
       end
