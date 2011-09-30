f062ce620b9dc35cd3dab8e90d15fde7cb011f6c
Tom Coleman <tom@thesnail.org>
2011-09-30 18:37:46 +1000

    Make sure it works with action_mailer

--- lib/edifice.rb
+++ lib/edifice.rb
@@ -17,6 +17,6 @@ module Edifice
 end
 
 ActionController::Base.send :include, Edifice::Controller
-# ActionMailer::Base.send :include, Edifice::Controller
+ActionMailer::Base.send :include, Edifice::Controller
 ActionView::Base.send :include, Edifice::Helper
 ActionView::Renderer.send :include, Edifice::Renderer
