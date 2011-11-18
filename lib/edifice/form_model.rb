8a3e22ef857bbbfe34d0f2de829dece9bc0be046
Tom Coleman <tom@thesnail.org>
2011-11-18 11:31:32 +1100

    Added a save! method to form_model

--- lib/edifice/form_model.rb
+++ lib/edifice/form_model.rb
@@ -6,6 +6,16 @@ module Edifice
     include ActiveModel::Conversion
     extend  ActiveModel::Naming
     
+    # more or less the same as activerecord's one
+    class RecordInvalid < Exception
+      attr_reader :record
+      def initialize(record)
+        @model = record
+        errors = @record.errors.full_messages.join(", ")
+        super(errors)
+      end
+    end
+    
     def initialize(attributes = {})
         attributes.each { |n, v| send("#{n}=", v) if respond_to?("#{n}=") }
     end
@@ -13,6 +23,10 @@ module Edifice
     # default implementation, override as necessary
     def save
       valid?
+    end
+    
+    def save!
+      save || raise(RecordInvalid.new(self))
     end
     
     def self.create(attributes = {})
