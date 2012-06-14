8e0df21ac140875c18d98da5a9cb3752aa403c3d
Tom Coleman <tom@thesnail.org>
2012-06-14 17:00:48 +1000

    Fixed to work in rails3.2

--- lib/edifice/renderer.rb
+++ lib/edifice/renderer.rb
@@ -11,10 +11,13 @@ module Edifice
 
   class TemplateRenderer < ActionView::TemplateRenderer
     def render_template(template, layout_name = nil, locals = {})
+      layout_name = layout_name.call if layout_name.is_a? Proc
+      layout = if layout_name.is_a? String then layout_name.split('/').last else '' end
+      
       # ensure we aren't rendering e.g. text
       if template.respond_to?(:virtual_path) and @view.controller and @view.controller.respond_to?(:set_edifice_names)
         @view.controller.set_edifice_names(template.virtual_path.name, 
-          template.virtual_path.prefix, (layout_name.nil? ? '' : layout_name.split('/').last))
+          template.virtual_path.prefix, layout)
       end
       
       super(template, layout_name, locals)
